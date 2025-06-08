97f93b0317a942b463228459eda3af797f9f6c92
Mou2004 <92316657+Mou2004@users.noreply.github.com>
2025-06-08 12:57:49 +0200

    update merge method in MergeSort Class

--- DivideAndConquer/src/MergeSort.java
+++ DivideAndConquer/src/MergeSort.java
@@ -7,25 +7,28 @@ public class MergeSort {
     * 2. MergeSort(left)+MergeSort(right)=sort the left part and right part of the array
     * 3. Combine/Merge the left right solutions
     * */
-    public static void mergeSort(int arr[], int si, int ei){
-        //basecase
-        if (si>=ei){
-            return;
+    public static void mergeSort(int arr[], int startIndex, int endIndex){
+        //basecase i.e  when the array has one or less elements
+        if (startIndex>=endIndex){
+            return; //1 element is already sorter
         }
 
-        int mid = si+(si-ei)/2;//=(si+ei)/2
-        mergeSort(arr,si,mid);//left part
-        mergeSort(arr, mid+1,ei);//right part
-        merge(arr,si,mid,ei);
+        int mid = startIndex    +(startIndex-endIndex)/2;//=(si+ei)/2; use this when si and ei are small, large values the first formula is better
+        mergeSort(arr,startIndex,mid);//sort left part
+        mergeSort(arr, mid+1,endIndex);//sort right part
+        merge(arr,startIndex,mid,endIndex);// merge both parts by comparing each element
     }
-    public static void merge(int arr[],int si,int mid, int ei){
-        int[] temp = new int[ei-si+1];
-        int i = si;//iterator for left part
+
+    //this method merges the sorted parts
+    public static void merge(int arr[],int startIndex,int mid,  int endIndex){
+        int[] temp = new int[endIndex-startIndex+1];//size of the temp array would ei-si+1, cause array indexing starts at 0
+        int i = startIndex;//iterator for left part
         int j = mid+1;//iterator for right part
-        int k = 0;//iterator for temp array
-        while(i<=mid&&j<=ei){
-            if (arr[i]<arr[j]){
-                temp[k]=arr[i];
+        int k = 0;//index iterator for temp array
+        while(i<=mid&&j<=endIndex)// iterating till the end of both left and right parts
+            {
+            if (arr[i]<arr[j]){ //comparing the corresponding index elements of the right and left parts
+                temp[k]=arr[i]; //storing the smaller element in the temp array
                 i++;
             }else{
                 temp[k]=arr[j];
@@ -35,10 +38,31 @@ public class MergeSort {
             k++;
 
         }
+        //for leftover elements of the first sorted part
+        while(i<mid){
+            temp[k++]=arr[i++];
+        }
 
+        //for leftover elements of 2nd sorted part
+        while(j <= endIndex) {
+            temp[k++] = arr[j++];
+        }
+
+        //copy temp to original array
+        for(k=0, i=startIndex; k<temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+    public static void printArr(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] +" ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
-
+        int arr[] = {6, 3, 9, 5, 2, 8};
+        mergeSort(arr, 0, arr.length-1);
+        printArr(arr);
     }
 }
